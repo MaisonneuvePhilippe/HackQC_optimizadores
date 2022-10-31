590f10a71d1c09c8533247dd2b076ebf236fdddf
jupall <julien.pallage@polymtl.ca>
2022-10-31 01:24:14 -0400

    new datasets and tests api

--- brainstorm.py
+++ brainstorm.py
@@ -2,11 +2,12 @@
 """
 Created on Sun Oct 30 13:09:52 2022
 
-@author: jgpal
+@author: Julien Pallage
 
 """
 
 import pandas as pd
+import numpy as np
 import json
 import requests
 import urllib3
@@ -32,7 +33,6 @@ class EV :
         self.year = year
         self.battery = self.batterySizeData(brand, model, year)
          
-
 myEV1 = EV("Nissan", "Leaf", 2019)
 i = evdf.loc[(evdf["brand"] == "Nissan") & (evdf["model"] == "Leaf") & (evdf["release_year"] == 2019)].index
 evdf.usable_battery_size[i[0]]
@@ -41,10 +41,9 @@ evdf.usable_battery_size[i[0]]
 
 
 
-
 ## TEST WITH API FROM DONNEES QUEBEC
 
-# API test for QUARTER HOURLY demand dataset given by Hydro-Quebec:
+## API test for QUARTER HOURLY demand dataset given by Hydro-Quebec:
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/demande.json')
 dataset = json.loads(r.data.decode('utf-8'))
@@ -52,21 +51,72 @@ dataset = json.loads(r.data.decode('utf-8'))
 def get_recent_demand(dataset_HQ):
     return dataset_HQ["details"][dataset_HQ["indexDonneePlusRecent"]]["valeurs"]["demandeTotal"]
     
+# gives the past n demands (increments of 15 minutes) in chronological order
+def get_n_last_demand(dataset_HQ, n):
+    lastIndex = dataset_HQ["indexDonneePlusRecent"]
+    ndemands = []
+    test = lastIndex - n + 1
+    test = test if test >= 0 else 0
+    for i in np.arange(test, lastIndex + 1) : 
+        ndemands.append(dataset_HQ["details"][i]["valeurs"]["demandeTotal"])      
+    return ndemands
+    
 
-# API test for HOURLY information about importations, exportations and GES 
 
+
+## API test for HOURLY information about importations, exportations and GES 
 r = http.request('GET', "https://donnees.solutions.hydroquebec.com/donnees-ouvertes/data/json/ges-electricite.json")
 dataset = json.loads(r.data.decode('utf-8'))
 
+# note : no easy way to find the last update in the dataset, this is a quick fix
+# but doesn't consider exceptions when changing months or years
 def get_recent_market(dataset):
     qcTime = pytz.timezone("US/Eastern") 
     timeInQc = datetime.now(qcTime)
-    lastUpdate = f"{timeInQc.year}-{timeInQc.month}-{timeInQc.day}T{timeInQc.hour}:00:00"
+    if timeInQc.hour == 0 and timeInQc.minute <= 35:
+        lastUpdate = f"{timeInQc.year}-{timeInQc.month}-{timeInQc.day - 1}T23:00:00"
+    elif timeInQc.minute < 35:
+        lastUpdate = f"{timeInQc.year}-{timeInQc.month}-{timeInQc.day}0T{timeInQc.hour - 1 : 02}:00:00"
+    else: 
+        lastUpdate = f"{timeInQc.year}-{timeInQc.month}-{timeInQc.day}T{timeInQc.hour : 02}:00:00"
     return [dictionary for dictionary in dataset["details"] if dictionary["date"] == lastUpdate ][0]
-    
-    
-dic = get_recent_market(dataset)
 
-# API for quarter hourly production :
+HIGH_GES = 50000 # total des émissions directes de GES, en kilogrammes d’équivalent CO2 (kg) 
+# returns true if GES due to power consumption goes higher than HIGH_GES
+# could send a notification to the user if return = True
+def high_ges_alert(dataset):
+    lastMarket = get_recent_market(dataset)
+    if lastMarket["Quebec_Consommation_GES"]["total"] > HIGH_GES:
+        return True
+    else:
+        return False
+    
+    
+## API for bi-hourly production :
+r = http.request('GET', "https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/production.json")
+dataset = json.loads(r.data.decode('utf-8'))  
+
+# returns only the total energy production
+def get_recent_production_total(dataset_HQ):
+    return dataset_HQ["details"][dataset_HQ["indexDonneePlusRecent"]]["valeurs"]["total"]
+    
+# gives the past n total power (increments of 30 minutes) in chronological order
+def get_n_last_production_total(dataset_HQ, n):
+    lastIndex = dataset_HQ["indexDonneePlusRecent"]
+    nprod = []
+    test = lastIndex - n + 1
+    test = test if test >= 0 else 0 # if index is negative, start at 0
+    for i in np.arange(test, lastIndex + 1) : 
+        nprod.append(dataset_HQ["details"][i]["valeurs"]["total"])      
+    return nprod
+
+def get_recent_production_sources(dataset_HQ):
+   return  dataset_HQ["details"][dataset_HQ["indexDonneePlusRecent"]]["valeurs"]
+
+
+# API for debit rivieres :
+r = http.request('GET', "https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/Donnees_VUE_CENTRALES_ET_OUVRAGES.json")
+dataset = json.loads(r.data.decode('utf-8'))    
+    
     
     
