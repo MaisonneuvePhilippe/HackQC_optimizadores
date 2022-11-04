b95281caa0e96d356aa23101e5520e51b80caa7b
PhilippeMaisonneuve <philippemaisonneuve@pop-os.localdomain>
2022-11-04 04:41:29 -0400

    many city in data

--- webscraping.py
+++ webscraping.py
@@ -3,11 +3,16 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+CITY = {
+    "Montreal": "https://weather.com/fr-CA/temps/parheure/l/623267a6cbae200f3c71ffba3e29de9696db705ae363efd10c32b3072fa43af4",
+    "Quebec": "https://weather.com/fr-CA/temps/parheure/l/9e8fe4026d99ec5975cd95d3fb890e1c93041034b1a243aa640d65d2b90d9441",
+    "Gatineau": "https://weather.com/fr-CA/temps/parheure/l/03eb9f889bd959afda59325f2a5ca107c5f6914bd97408accb62cd668acfffdc",
+    "Sheerbrooke": "https://weather.com/fr-CA/temps/parheure/l/cc768c900e8a81e07fc501e9021b2350793db46ac55ff74180e594143b491ea9",
+}
 
-def get_meteo(n):
-    html_text = requests.get(
-        "https://weather.com/fr-CA/temps/parheure/l/623267a6cbae200f3c71ffba3e29de9696db705ae363efd10c32b3072fa43af4"
-    ).text
+
+def get_meteo(n, city):
+    html_text = requests.get(CITY[city]).text
 
     items = []
     soup = BeautifulSoup(html_text, "lxml")
